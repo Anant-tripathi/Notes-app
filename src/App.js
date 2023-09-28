40ecfe8e2b8459d9f6be0a60820b946815fe744a
Anant Tripathi <anantraj.tripathi79@gmail.com>
2023-09-28 20:11:48 +0530

    created grid layout for application

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="sidebar">
+        <ul>
+          <li>item</li>
+          <li>item</li>
+          <li>item</li>
+          <li>item</li>
+          <li>item</li>
+          <li>item</li>
+        </ul>
+      </div>
+      <div className="main"></div>
+    </div>
+  );
+}
+
+export default App;
