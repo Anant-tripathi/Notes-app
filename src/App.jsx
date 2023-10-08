ab162d78b1fab2a7d2b4c6754cc049384ef03639
Anant Tripathi <anantraj.tripathi79@gmail.com>
2023-10-08 17:23:47 +0530

    added basic note taking functionality and adding new notes via a button

--- src/App.jsx
+++ src/App.jsx
@@ -15,50 +15,83 @@ function App() {
     },
     {
       id: 3,
-      name: "somenam3",
+      name: "somename3",
       text: "sometex3",
     },
+    {
+      id: 4,
+      name: "",
+      text: "",
+    },
   ]);
-  const [selectedItem, setSelectedItem] = useState(null);
-  console.log(selectedItem);
+
+  const [selectedItem, setSelectedItem] = useState(3);
 
   return (
     <div className="container">
-      <Sidebar items={items} setSelectedItem={setSelectedItem} />
-      <Main items={items} setItems={setItems} />
+      <Sidebar
+        items={items}
+        setItems={setItems}
+        selectedItem={selectedItem}
+        setSelectedItem={setSelectedItem}
+      />
+      <Main items={items} setItems={setItems} selectedItem={selectedItem} />
     </div>
   );
 }
 
-function Sidebar({ items, setSelectedItem }) {
+function Sidebar({ items, setItems, selectedItem, setSelectedItem }) {
+  function handleAddNote() {
+    const newNote = {
+      id: items[selectedItem].id + 1,
+      name: "",
+      text: "",
+    };
+    setItems([...items, newNote]);
+    setSelectedItem(newNote.id - 1);
+  }
+
   return (
     <div className="sidebar">
-      <ul>
+      <ul className="list">
         {items.map((item) => (
-          <Notes key={item.id} item={item} />
+          <Notes key={item.id} item={item} setSelectedItem={setSelectedItem} />
         ))}
       </ul>
+      <button className="addNote" onClick={handleAddNote}>
+        ADD
+      </button>
     </div>
   );
 }
 
 function Notes({ item, setSelectedItem }) {
   const selectItem = () => {
-    console.log("hi");
-    setSelectedItem = item.id;
+    setSelectedItem(item.id - 1);
   };
-  return <li onClick={() => selectItem}>{item.name}</li>;
+  return (
+    <li className="noteItem" onClick={selectItem}>
+      {item.name}
+    </li>
+  );
 }
 
-function Main({ items, setItems }) {
+function Main({ items, setItems, selectedItem }) {
+  const handleNoteUpdate = (e) => {
+    const updatedItems = [...items];
+    updatedItems[selectedItem].text = e.target.value;
+    updatedItems[selectedItem].name = e.target.value.slice(0, 20);
+    setItems(updatedItems);
+  };
+
   return (
     <div className="main">
       <h1>Notes App</h1>
       <form action="submit">
         <textarea
-          value={items[1].text}
+          value={items[selectedItem].text}
           className="input"
-          onChange={(e) => items[1].setText(e.target.value)}
+          onChange={handleNoteUpdate}
         />
       </form>
     </div>
